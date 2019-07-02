b070d82903d1c2c6762372df6391f753a04a57b1
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-07-02 17:31:21 -0500

    structure created for pet class and game object, mvp

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//game object 
+
+//initiate the pet, in the game object. 
+//start game by naming the pet with prompt and initiate the pet. 
+//buttons feed, light on and off, play each should update the pet. 
+//pet died if any of the scale hits 10 
+//increase the age. 
+//morph the pet at certain age.
+
+
+//user interactivity with the game
+const game = {
+	//is the time the player object? 
+	pet: {},
+
+	createPet(){
+		//name should be a prompt 
+	},
+
+	feedPet(){
+
+	},
+
+	controlLight(){
+
+	},
+
+	playWithPet(){
+
+	},
+
+}
---
+++ js/pets.js
@@ -0,0 +1,40 @@
+//class
+
+// each pet should have hunger(1-10), sleepiness(1-10), boredom(1-10), age, name, 
+//the increase function should be in object. 
+
+class Pets {
+	constructor(name){
+		this.name = name 
+		this.age = 1
+		this.sleepiness = 1
+		this.boredom = 1
+	}
+//those functions are not controlled by the users, thus should be created in the pet class
+	initPet(){
+		//attach the pet to the screen, 
+		// have all the functions below 
+	}
+
+	increaseHunger(){
+		//function should attach this to the html 
+		//function should increase its hunger 
+	}
+
+	increaseSleepiness(){
+		// attach stuff to the screen 
+		//increase 
+	}
+
+	increaseAge(){
+		//morph the age ? 
+	}
+
+	increaseBoredom(){
+
+	}
+
+	increaseTime(){
+
+	}
+}
